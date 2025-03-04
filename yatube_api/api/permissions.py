b612ca45bb333f65f3572f614ab0d2c864faefb0
Dzmitry Radziuk <mitia.radiuk@yandex.ru>
2025-03-04 12:40:13 +0300

    api_yatube_final

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,10 +3,6 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """Изменять или удалять ресурс может только автор."""
-    # У меня только вот с этим вариантом тесты проходит, если вьюхи не менять.
-    # Если можно, напишите, как бы Вы сделали, в комментах к ревью
-    # или в пачке.
-    # Спасибо за подробные комментарии и полезные ссылки!)
     def has_object_permission(self, request, view, obj):
         """Разрешает безопасные методы всем, а изменение — только автору."""
         if request.method in permissions.SAFE_METHODS:
